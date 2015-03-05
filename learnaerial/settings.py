d3a6022b2bf0c3ee9d182bf14be2c33b02bf420e
Lisa Jamhoury <lisajamhoury@gmail.com>
2015-03-05 21:43:14 +0200

    open graph url

--- learnaerial/settings.py
+++ learnaerial/settings.py
@@ -66,6 +66,19 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'main.context_processors.get_current_path)',
+)
+
 ROOT_URLCONF = 'learnaerial.urls'
 
 WSGI_APPLICATION = 'learnaerial.wsgi.application'
