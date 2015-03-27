d445e1ed18b590ad4b77763a866134176f4f65cc
Lisa Jamhoury <lisajamhoury@gmail.com>
2015-03-26 22:47:11 -0400

    adds og images specific to each event

--- main/context_processors.py
+++ main/context_processors.py
@@ -1,4 +1,6 @@
 def get_current_path(request):
+    base_url = 'http://' + request.get_host()
     return {
-       'current_path': request.build_absolute_uri()
+       'current_path': request.build_absolute_uri(),
+       'base_url' : base_url
      }
