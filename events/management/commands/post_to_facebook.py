4d98c34a25cc2b32f55c52485e2945955498f404
Lisa Jamhoury <lisajamhoury@gmail.com>
2015-05-13 21:23:27 -0400

    adds debugger to facebook autoposter

--- events/management/commands/post_to_facebook.py
+++ events/management/commands/post_to_facebook.py
@@ -9,12 +9,14 @@ class Command(BaseCommand):
     help = 'Posts unposted events to Facebook'
 
     def handle(self, *args, **options):
+        print "running facebook autoposter"
         graph = facebook.GraphAPI(settings.FACEBOOK_PAGE_TOKEN)
-        
+
         events_to_post = Event.objects.filter(post_to_facebook=True, posted_to_facebook=False)
 
         for event in events_to_post:
             if event.ready_to_post():
+            	print "posted event: %s" % event 
                 graph.put_object(settings.FACEBOOK_PAGE_ID, "feed", link=event.get_full_url()) 
                 event.posted_to_facebook = True
                 event.save()
