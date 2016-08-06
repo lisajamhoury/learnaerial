6d40e87a93e0ad461aa02a8bfeb93eef09233683
Lisa Jamhoury <lisajamhoury@gmail.com>
2016-08-06 17:26:54 -0400

    Change listing city filtering to OR from AND

--- frontend/learnaerial/bower_components/listings.js
+++ frontend/learnaerial/bower_components/listings.js
@@ -1,79 +1,95 @@
 var GRID = {
-	initialize: function() {
-		GRID.grid = $('#listings_grid').isotope({
-			itemsSelector: 'li',
-		});
+  initialize: function() {
+    GRID.grid = $('#listings_grid').isotope({
+      itemsSelector: 'li',
+    });
 
-		//get all the checkboxes
-		var checkboxes = $('input[type=checkbox]');
+    //get all the checkboxes
+    var checkboxes = $('input[type=checkbox]');
 
-		//when checkbox clicked, we update filters
-		checkboxes.on('change', GRID.updateFilters);
-	},
+    //when checkbox clicked, we update filters
+    checkboxes.on('change', GRID.updateFilters);
+  },
 
-	updateFilters: function(event) {
-		event.preventDefault();
+  updateFilters: function(event) {
+    event.preventDefault();
 
-		// Using similar code as above get all checked inside categories
-		var checkedCategories = $('.categories input[type=checkbox]:checked');
+    // Using similar code as above get all checked inside categories
+    var checkedCategories = $('.categories input[type=checkbox]:checked');
 
-		// Using similar code as above get all checked inside offerings
-		var checkedOfferings = $('.offerings input[type=checkbox]:checked');
+    // Using similar code as above get all checked inside offerings
+    var checkedOfferings = $('.offerings input[type=checkbox]:checked');
 
-		// Using similar code as above get all checked inside cities
-		var checkedCities = $('.location input[type=checkbox]:checked');
+    // Using similar code as above get all checked inside cities
+    var checkedCities = $('.location input[type=checkbox]:checked');
 
-		console.log(checkedCategories);
-		console.log(checkedOfferings);
-		console.log(checkedCities);
+    // console.log(checkedCategories);
+    // console.log(checkedOfferings);
+    // console.log(checkedCities);
 
-		// Get the filter names for each group of checked checkboxes
+    // Get the filter names for each group of checked checkboxes
 
-		var categoriesFilterList = GRID.getFilterListFromChecked(checkedCategories);
-		var offeringsFilterList = GRID.getFilterListFromChecked(checkedOfferings);
-		var citiesFilterList = GRID.getFilterListFromChecked(checkedCities);
+    var categoriesFilterList = GRID.getFilterListFromChecked(checkedCategories);
+    var offeringsFilterList = GRID.getFilterListFromChecked(checkedOfferings);
+    var citiesFilterList = GRID.getFilterListFromChecked(checkedCities);
 
-		console.log(categoriesFilterList);
-		console.log(offeringsFilterList);
-		console.log(citiesFilterList);
+    // Add categories and offerings
+    var allFilterList = categoriesFilterList.concat(offeringsFilterList);
 
-		var allFilterList = categoriesFilterList.concat(offeringsFilterList, citiesFilterList);
+    // If city filters checked, create one query for each city
+    if (citiesFilterList.length) {
+      var collectFilters = [];
+      var allFilters;
 
-		var allFilters = allFilterList.join('');
-		
-		console.log(allFilters);
-		GRID.grid.isotope({ filter: allFilters });
+      for (i = 0; i < citiesFilterList.length; i++) {
+        var eachCityFilterList = allFilterList.concat(citiesFilterList[i]);
+        var eachCityFilter = eachCityFilterList.join('');
 
-	},
+        // Create array of filters per city
+        collectFilters.push(eachCityFilter);
+      }
 
-	getFilterListFromChecked: function(checked) {
-		// This function accepts an array of input boxes, and returns an array of strings
-		// that correspond to the filter that will be used in Isotope
-		// Given a list of inputs like this:
-		// [<input type="checkbox" data-filter-group="city" data-filter-slug="new-york">, [<input type="checkbox" data-filter-group="city" data-filter-slug="frenchtown">,]
-		// We return:
-		// ['.city-new-york', '.city-frenchtown']
+      // Join filters with comma separation to indicate OR for each city
+      allFilters = collectFilters.join(', ');
 
-		// To do this, we must first LOOP through all of the things inside the 'checked' variable
-		// USE THIS: http://api.jquery.com/jquery.each/
+    } else {
+      // Otherwise just join all filters
+      allFilters = allFilterList.join('');
+    }
 
-		// Create a variable that will store your results
+    GRID.grid.isotope({
+      filter: allFilters
+    });
 
-		var filterList = [];
+  },
 
-		// Now start your LOOP here
-		$.each(checked, function( index, value ) {
-			console.log('loooooooooooooooooooooooooooop');
+  getFilterListFromChecked: function(checked) {
+    // This function accepts an array of input boxes, and returns an array of strings
+    // that correspond to the filter that will be used in Isotope
+    // Given a list of inputs like this:
+    // [<input type="checkbox" data-filter-group="city" data-filter-slug="new-york">, [<input type="checkbox" data-filter-group="city" data-filter-slug="frenchtown">,]
+    // We return:
+    // ['.city-new-york', '.city-frenchtown']
 
-			// Inside your loop, you first make the string
-			var filter = '.' + $(value).data('filter-group') + '-' + $(value).data('filter-slug');
-			console.log(filter);
-			 // Jquery Magic to get the value for data-filter-group and value for data-filter-slug hint: https://api.jquery.com/jquery.data/
+    // To do this, we must first LOOP through all of the things inside the 'checked' variable
+    // USE THIS: http://api.jquery.com/jquery.each/
 
-			filterList.push(filter);
-		});
+    // Create a variable that will store your results
 
-		return filterList
+    var filterList = [];
 
-	}
+    // Now start your LOOP here
+    $.each(checked, function(index, value) {
+
+      // Inside your loop, you first make the string
+      var filter = '.' + $(value).data('filter-group') + '-' + $(value).data('filter-slug');
+      //console.log(filter);
+      // Jquery Magic to get the value for data-filter-group and value for data-filter-slug hint: https://api.jquery.com/jquery.data/
+
+      filterList.push(filter);
+    });
+
+    return filterList
+
+  }
 };
